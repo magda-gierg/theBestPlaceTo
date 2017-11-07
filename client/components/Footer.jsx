7579764c126f49859603686325dc406630c6290a
magda-gierg <mjgiergiczny@gmail.com>
2017-11-08 12:02:22 +1300

    Footer component

---
+++ client/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='container'>
+        <div className='footer-text'>
+          <a href='/' style={{color: '#fff'}}>
+            <span className='the best'>the best place to...</span>
+            <p className='footer-slogan'>Thank you for visiting!</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
