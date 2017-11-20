63caa9abdf6b06009e0f7817c66abe15cbc1e6be
magda-gierg <mjgiergiczny@gmail.com>
2017-11-21 09:18:51 +1300

    SearchBar in progress

--- client/containers/SearchBar.jsx
+++ client/containers/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import {connect} from 'react-redux'
+import {connect} from 'react-redux'
 
 import SearchResults from './SearchResults'
 import {getSports} from '../actions/sports'
@@ -12,27 +12,82 @@ class SearchBar extends React.Component {
       sports: props.sports,
       placesBySport: props.placesBySport,
       searchSport: '',
-      searchResults: []
+      searchResults: [],
+      showPlaces: false,
+      sortedPlaces: []
+
     }
   }
 
-
-
-render() {
-  const {sports, dispatch} = this.props
-  return (
-    <div className='container search-bar-body'>
-      <div>
-        <h4 className={this.state.searchSport.length == 0 ? 'search-bar-header-ready animated bounceIn':"search-bar-header"}>Pick the right sport for you!</h4>
+  showPlaces (selectedSport) {
+    let sortedPlaces = this.state.placesBySport.filter((sport) => {
+      return sport.sport_id == selectedSport.sport_id
+    })
+    this.setState({sortedPlaces, showPlaces: true})
+  }
+  renderPlaces () {
+    return (
+      <div className='search-results-places' >
+        <h5>the best places to do {sport.sport_id}:</h5>
+        <SearchResults places={this.state.sortedPlaces}/>
       </div>
-      <form className={this.state.searchSport.length == 0 ? 'search-box-ready':'search-box'}>
-        <input placeholder='Search' type='text' onChange={(e) => this.searchHandler(e)}></input>
-      </form>
+    )
+  }
+
+  componentDidMount () {
+    this.props.dispatch(getSports())
+    this.props.dispatch(getPlaceBySport())
+    this.searchHandler = this.searchHandler.bind(this)
+  }
+
+  searchHandler (e) {
+    this.setState({
+      searchSport: e.target.value,
+      searchResults: this.filterSearchSports(e.target.value),
+      showPlaces: false,
+      sortedPlaces: []
+    })
+  }
+  componentWillReceiveProps ({placesBySport, sports}) {
+    this.setState({placesBySport, sports})
+  }
+  filterSearchSports (searchTerm) {
+    if (searchTerm == '' || !searchTerm) return []
+    return this.state.sports.filter((sport) => {
+      return sports.sport_name.toLowerCase().includes(searchTerm.toLowerCase())
+    })
+  }
+  renderSportInfo (sport, key) {
+    return <div className="search-result" key={key}>
+      <button onClick={() => this.showPlace(sport)}>{sport.sport_name}</button>
+    </div>
+  }
+
+
+  render() {
+    const {sports, dispatch} = this.props
+    return (
+      <div className='container search-bar-body'>
+        <div>
+          <h4 className={this.state.searchSport.length == 0 ? 'search-bar-header-ready animated bounceIn':"search-bar-header"}>Pick the right sport for you!</h4>
+        </div>
+        <form className={this.state.searchSport.length == 0 ? 'search-box-ready':'search-box'}>
+          <input placeholder='Search' type='text' onChange={(e) => this.searchHandler(e)}></input>
+        </form>
+        <div className='search-results-list'>
+          {this.state.searchResults.map((sport, key) => this.renderSportInfo(sport, key))}
+        </div>
+
+        {this.state.showPlaces && this.renderPlaces()}
       </div>
     )
   }
 }
 
+const mapStateToProps = (state, other) => {
+  return {sports: state.sports,
+    places: state.places
+  }
+}
 
-
-export default SearchBar
+export default connect(mapStateToProps)(SearchBar)
